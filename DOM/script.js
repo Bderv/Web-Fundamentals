2b8e23ee5458cfcd389afc8e2d37b1c288b2238c
Bderv <67141305+Bderv@users.noreply.github.com>
2020-10-21 11:09:06 -0500

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,13 @@
+console.log(document)
+
+var h1 = document.getElementsByTagName("h1");
+console.log(h1)
+
+var title = document.getElementById("title");
+console.log(title.innerHTML)
+title.innerHTML="this is fun"
+
+var boxen = document.getElementsByClassName("box")
+console.log(boxen);
+
+for
